3b6b53a3dda02b17c9ac7ee7202a278c6454e7e9
AleixMT <aleixaretra@gmail.com>
2017-09-26 09:24:05 +0200

    changes

--- OrdenacioCercaCost/main.c
+++ OrdenacioCercaCost/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <time.h>
 #define N 20
+
 
 void inicialitza(int v[N])
 {
@@ -19,11 +21,44 @@ void visualitza(int v[N])
         printf("%i, ", v[i]);
     }
 }
+
+void seleccio()
+{
+    printf ("hoal tetis");
+}
+
+void bombolla()
+{
+    printf ("end of funccncncncnnntionn");
+}
+
+void insercio()
+{
+    printf ("inxerxion");
+}
+
+int opcio ()
+{
+    int c=0;
+    while (1){
+    printf ("\nQuin algoritme vols fer servir?\n1.-Selecció\n2.-Bombolla\n3.-Inserció\n\t\t->");
+    c = getche();
+    if (c < 49 || c > 51) printf ("\n\nOpció incorrecta, torna-ho a intentar...");
+    else break;
+    }
+    return (c - 48);
+}
 int main()
 {
     int v[N], cmpt;
-
     inicialitza(v);
     visualitza(v);
+    switch (opcio())
+    {
+        case 1: seleccio();
+        case 2: bombolla();
+        case 3: insercio();
+    }
+
     getch();
 }
