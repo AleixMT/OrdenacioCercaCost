85ff1281afad791947836ea021bb09fcc70c9dee
toful <tofuldauden@gmail.com>
2017-09-26 09:24:58 +0200

    Toful: selection sort

--- OrdenacioCercaCost/main.c
+++ OrdenacioCercaCost/main.c
@@ -1,29 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 #define N 20
 
-void inicialitza(int v[N])
+
+
+void inicialitza(unsigned int v[], int lenght)
 {
     int i;
     srand(time(NULL));
-    for (i = 0; i < N; i++) v[i] = (rand()%(N*100));
+    for (i = 0; i < lenght; i++) v[i] = rand()%(lenght*100);
 }
 
-void visualitza(int v[N])
+void visualitza(unsigned int v[], int lenght)
 {
     int i;
-    printf ("Vector amb %i elements\n", N);
-    for (i = 0; i < N; i++)
+    printf ("Vector amb %i elements\n", length);
+    for (i = 0; i < length; i++)
     {
-        printf("%i, ", v[i]);
+        printf("%d, ", v[i]);
     }
 }
+
+void swap(unsigned int v[], int i, int j){
+    int a=v[i];
+    v[i]=v[j];
+    v[j]=a;
+}
+
+void selection_sort(unsigned int v[], int length){
+    int j, i;
+
+    for (j = 0; j < length-1; j++){
+        int iMin = j;
+        for (i = j+1; i < length; i++) {
+            if (v[i] < v[iMin]) {
+                iMin = i;
+            }
+        }
+        if(iMin != j){
+            swap(v[j], v[iMin]);
+        }
+    }
+
+}
+
+
 int main()
 {
-    int v[N], cmpt;
-
-    inicialitza(v);
+    unsigned int v[N];
+    inicialitza(v, N);
     visualitza(v);
-    getch();
 }
